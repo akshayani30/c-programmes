309f843fc1b788f72f892df910bf1885254544cb
akshayani30 <agithaakshayani@gmail.com>
2024-12-27 00:51:45 +0530

    created ak file

--- areaofcircle.c
+++ areaofcircle.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 //area of cicle
+//akkkkkk
 int main()
 {
     int radius;
     scanf("%d", &radius);
     printf("%d", 3.14 * radius* radius);
+printf("hello bava");
+
     return 0;
 }
